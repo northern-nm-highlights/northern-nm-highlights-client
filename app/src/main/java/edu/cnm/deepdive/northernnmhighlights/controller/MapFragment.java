968c9c88c52c8c5ab3c43525b4e425d2a025180b
nmitchell313 <nmitchell11@cnm.edu>
2021-12-10 15:15:55 -0700

    map fragment

--- app/src/main/java/edu/cnm/deepdive/northernnmhighlights/controller/MapFragment.java
+++ app/src/main/java/edu/cnm/deepdive/northernnmhighlights/controller/MapFragment.java
@@ -1,17 +1,13 @@
 package edu.cnm.deepdive.northernnmhighlights.controller;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
@@ -19,7 +15,6 @@ import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import edu.cnm.deepdive.northernnmhighlights.databinding.FragmentMapBinding;
-import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 public class MapFragment extends Fragment implements OnMyLocationButtonClickListener,
